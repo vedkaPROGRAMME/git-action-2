f8fcb23962cad59fd50bb1e7c8330fd273058dba
vedkaPROGRAMME <vedantgsalunkhe@gmail.com>
2026-01-16 11:44:44 +0530

    test

---
+++ test/required-tokens.spec.js
@@ -0,0 +1,11 @@
+"use strict";
+
+const assert = require("node:assert");
+const { describe, it } = require("../..");
+
+describe('using imported "describe"', function () {
+  it('using imported "it"', function (done) {
+    assert.ok(true);
+    done();
+  });
+});
